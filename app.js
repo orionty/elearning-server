55582de3418fa0a15fc8ea87ecf239b54c1a4619
Okyere Amponsah Kwatia <77788839+orionty@users.noreply.github.com>
2022-10-10 11:56:51 -0700

    app

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(cors())
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://elearning-server-app.herokuapp.com",
     methods: ["GET", "POST"],
     
   },
